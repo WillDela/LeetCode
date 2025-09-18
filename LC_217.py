1eb3a0021386367d8c1973f21575b4f1d75c1caf
William Delaosa <wdelaosa24@gmail.com>
2025-09-17 21:38:52 -0400

    just added notes

---
+++ LC_217.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]: return True
+        else: return False  
+
+
+# first leetcode problem and went with a brute force solution 
+
+# O(n log n), most optimal solution would be O(n) but getting there little by little
